73639125d2c4c7bf2d2873df9e5254c98a633794
mavanhou <marcvanhouten@outlook.com>
2022-07-26 14:02:13 +0200

    remove connectionstring

--- ConsoleApp1/DatabaseModel.cs
+++ ConsoleApp1/DatabaseModel.cs
@@ -8,8 +8,7 @@ public class PricingContext : DbContext
 {
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer("Server=tcp:sql-server-26867153.database.windows.net, 1433; Initial Catalog=azurepricingdb26867153; Persist Security Info=False; User ID=azureuser; Password=Pa9w0rD-26867153; MultipleActiveResultSets=False; Encrypt=True; TrustServerCertificate=False; Connection Timeout=30;");
-    }
-  
+        optionsBuilder.UseSqlServer(@"")
+
     public DbSet<Item>? PricingItems { get; set; }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Microsoft.EntityFrameworkCore;
 
-string sqlconnectionstring = "Server=tcp:sql-server-26867153.database.windows.net,1433;Initial Catalog=azurepricingdb26867153;Persist Security Info=False;User ID=azureuser;Password=Pa9w0rD-26867153;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
 using (var db = new PricingContext())
 {
+    db.PricingItems
     db.PricingItems.Add(new Item { currencyCode = "EUR" });
     db.SaveChanges();
 
