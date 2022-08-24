77c9278925cd3926fcf57a8e73d3cdfaa5231b8e
mavanhou <marcvanhouten@outlook.com>
2022-08-24 16:59:41 +0200

    add select box

--- WebSite/Controllers/HomeController.cs
+++ WebSite/Controllers/HomeController.cs
@@ -3,46 +3,105 @@ using System.Diagnostics;
 using WebSite.Models;
 using Newtonsoft.Json;
 using Microsoft.AspNetCore.ResponseCaching;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace WebSite.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private HttpClient apiclient; 
+        private List<SelectListItem> ListofCurrency;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            apiclient = new HttpClient();
+
+            ListofCurrency = new List<SelectListItem>()
+            {   new SelectListItem() { Text = "US Dollar ($)", Value = "USD"},
+                new SelectListItem() { Text = "Euro (€)", Value = "EUR"},
+                new SelectListItem() { Text = "British Pound (£)", Value = "GBP"}
+            };
         }
 
-       
-        public async Task<IActionResult> Index()
+        public async Task<PricingModel> GetPricingInformation(string Currency)
         {
-            string baseurl = "https://prices.azure.com/api/retail/prices?";
+            PricingModel model = new PricingModel();
+
+            string baseurl = $"https://prices.azure.com/api/retail/prices?currencyCode={Currency}";
             string selection = "&$filter=serviceName eq \'Virtual Machines\' and armRegionName eq \'westeurope\'";
             string url = baseurl + selection;
 
-            HttpClient client = new HttpClient();
-          
             using HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
-            HttpResponseMessage response = await client.SendAsync(request);
-             
+            HttpResponseMessage response = await apiclient.SendAsync(request);
+
             var content = await response.Content.ReadAsStringAsync();
 
 
             if (response.StatusCode.ToString() == "OK")
             {
-                PricingModel pricing = new PricingModel();
-                pricing = JsonConvert.DeserializeObject<PricingModel>(content);
-                
-                ViewModel model = new ViewModel();
-                model.Pricingskus = pricing.Items;
-                
-                return View(model);
+                model = JsonConvert.DeserializeObject<PricingModel>(content);
+
             }
 
-            return View();
+            return model;
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            PricingModel pricing = await GetPricingInformation("USD");
+
+            ViewModel model = new ViewModel();
+            model.Pricingskus = pricing.Items;
+
+            //Create value for dropdownlist
+
+            foreach (var item in ListofCurrency)
+            {
+                if (item.Value == "EUR")
+                {
+                    item.Selected = true;
+                }
+            }
+            model.ListofCurrency = ListofCurrency;
+                            
+            return View(model);
+          
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(ViewModel m)
+        {
+            PricingModel pricing = await GetPricingInformation(m.SelectedCurrency);
+
+            m.Pricingskus = pricing.Items;
+
+            //Create value for dropdownlist
+
+            foreach (var item in ListofCurrency)
+            {
+                if (item.Value == m.SelectedCurrency)
+                {
+                    item.Selected = true;
+                }
+            }
+            
+            m.ListofCurrency = ListofCurrency;
+
+            return View(m);
+
+        }
+
+        [HttpPost]
+        public IActionResult Select(ViewModel m)
+        {
+
+            return View("Selection", m.SelectedCurrency);
+        }
+
+
 
         public IActionResult Select()
         {
--- WebSite/Models/ViewModel.cs
+++ WebSite/Models/ViewModel.cs
@@ -1,9 +1,15 @@
-﻿namespace WebSite.Models
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WebSite.Models
 {
     public class ViewModel
     {
         public IEnumerable<PricingItem> Pricingskus { get; set; }
         public SelectionModel selection { get; set; }
+
+        public IEnumerable<SelectListItem> ListofCurrency { get; set; }
+
+        public string SelectedCurrency { get; set; }
 
     }
 }
